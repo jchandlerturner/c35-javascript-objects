6366ebeaa1ff6dfd688707278d0338aff37b5125
Chandler Turner <chandlerturner@Chandlers-MacBook-Air.local>
2019-08-26 15:54:44 -0500

    Completed lightning exercises on JS Objects

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// console.log("Chan you got this!");
+
+// const hamster = {
+//     fluffy: true,
+//     name: "Hamilton",
+//     age: 40,
+//     legs: 3,
+//     children: ["Peter Pan", "Captain Hook", "Sally"],
+//     nickname: "Ham Sir",
+//     chewing: function chew(what) {
+//         console.log("chewing", what);
+//     }
+// }
+
+// const familyMember = [
+//     Dad = {
+//         age: 40,
+//         job: "pirate",
+//         name: "Bob",
+//         dead: false,
+// },
+
+//     Mom = {
+//         age: 35,
+//         job: "hairdresser",
+//         name: "Denise",
+//         dead: false,
+// },
+//     Son = {
+//         age: 20,
+//         job: "barista",
+//         name: "Billy",
+//         dead: false,
+// },
+//     Daughter = {
+//         age: 15,
+//         job: "student",
+//         name: "Brittany", 
+//         dead: true
+//     },
+// ]
